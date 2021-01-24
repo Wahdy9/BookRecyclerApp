af885fb701118b97d3ba1c6ccebed5a268913a9f
Wahdy9 <kwahdy@gmail.com>
2021-01-24 11:16:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bookrecycler/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.bookrecycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+//TODO: - Splash Activity
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
